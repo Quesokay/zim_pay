35b20eea609969322347029e393d9a6931e19d4d
Victor Moyo <ques4235@gmail.com>
2026-04-16 11:24:35 +0200

    tells SQLite to use Write-Ahead Logging (WAL). This allows SQLite to process simultaneous reads and writes much better.

--- backend/ZimPay.Infrastructure/AppDbContext.cs
+++ backend/ZimPay.Infrastructure/AppDbContext.cs
@@ -12,6 +12,12 @@ namespace ZimPay.Infrastructure
         public DbSet<Transaction> Transactions { get; set; }
         public DbSet<Pass> Passes { get; set; }
 
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=wallet.db;Cache=Shared");
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
